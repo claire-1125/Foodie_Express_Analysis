3a590380b0c6c9ed731fae8d5884b3ca30da9e88
claire-1125 <seojungbin0106@gmail.com>
2025-04-21 16:31:04 +0900

    Feat: add a retention about holiday inflow

--- 8_retention_visit_interval.sql
+++ 8_retention_visit_interval.sql
@@ -206,3 +206,147 @@ CROSS JOIN week0_retain_long_users f
 ORDER BY week_diff
 
 
+
+
+
+/* 재방문+연휴유입 vs. 재방문+연휴외유입 리텐션 계산 */
+
+WITH dau_list AS (
+  SELECT
+    event_date,
+    COUNT(DISTINCT user_pseudo_id) AS dau,
+  FROM advanced.app_logs_cleaned_target
+  GROUP BY event_date
+)
+, order_cnt_list_d AS (
+  -- 일일 주문 유저 수
+  SELECT
+    event_date,
+    COUNT(DISTINCT user_id) AS order_users_cnt
+  FROM advanced.app_logs_cleaned_target
+  WHERE event_name = 'click_payment'
+  GROUP BY event_date
+)
+, dau_vs_order_user AS (
+  -- DAU와 일별 주문 유저 수 비교
+  SELECT
+    d.event_date,
+    d.dau,
+    o.order_users_cnt,
+    ROUND(SAFE_DIVIDE(o.order_users_cnt, d.dau) * 100, 3) AS order_ratio
+  FROM dau_list d
+  INNER JOIN order_cnt_list_d o ON d.event_date = o.event_date
+)
+, holiday AS (
+  -- 연휴 정의
+  SELECT
+    event_date AS holiday_date,
+    DATE_TRUNC(event_date, WEEK(MONDAY)) AS holiday_week
+  FROM dau_vs_order_user
+  WHERE order_ratio > 30
+)
+, revisit_user_logs AS (
+  -- 1) 재방문 유저 로그
+  SELECT
+    *
+  FROM advanced.app_logs_cleaned_target
+  WHERE 1=1
+  AND user_pseudo_id NOT IN (
+    SELECT user_pseudo_id
+    FROM advanced.app_logs_target_visit_seg
+    WHERE visit_interval_cat = 'one_day'
+  )
+)
+, week_diff_revisit_users AS (
+  -- 2) 사용자별 첫방문일, 방문일, 방문간격(주차) 추출
+  SELECT
+    user_pseudo_id,
+    first_date,
+    event_date,
+    DATE_DIFF(event_date, first_date, WEEK) AS week_diff,
+  FROM (
+  SELECT DISTINCT
+    user_pseudo_id,
+    MIN(event_date) OVER (PARTITION BY user_pseudo_id) AS first_date,
+    event_date,
+  FROM revisit_user_logs
+  )
+)
+, week_retain_holiday_users AS (
+  -- 3) 주차별 유저 수 카운팅
+  SELECT
+    week_diff,
+    COUNT(DISTINCT user_pseudo_id) AS retain_users
+  FROM week_diff_revisit_users
+  WHERE user_pseudo_id IN (
+    SELECT DISTINCT user_pseudo_id
+    FROM week_diff_revisit_users
+    WHERE first_date IN (SELECT holiday_date FROM holiday)
+  )  -- 재방문 + 연휴 유입
+  GROUP BY week_diff
+)
+, week0_retain_holiday_users AS (
+  -- 4) 유입주차 유저 수 카운팅
+  SELECT
+    ANY_VALUE(week_diff) AS week_diff,
+    COUNT(DISTINCT user_pseudo_id) AS first_users
+  FROM week_diff_revisit_users
+  WHERE week_diff = 0
+  AND user_pseudo_id IN (
+    SELECT DISTINCT user_pseudo_id
+    FROM week_diff_revisit_users
+    WHERE first_date IN (SELECT holiday_date FROM holiday)
+  )  -- 재방문 + 연휴 유입
+)
+, week_retain_normal_day_users AS (
+  -- 3) 주차별 유저 수 카운팅
+  SELECT
+    week_diff,
+    COUNT(DISTINCT user_pseudo_id) AS retain_users
+  FROM week_diff_revisit_users
+  WHERE user_pseudo_id IN (
+    SELECT DISTINCT user_pseudo_id
+    FROM week_diff_revisit_users
+    WHERE first_date NOT IN (SELECT holiday_date FROM holiday)
+  )  -- 재방문 + 연휴 외 유입
+  GROUP BY week_diff
+)
+, week0_retain_normal_day_users AS (
+  -- 4) 유입주차 유저 수 카운팅
+  SELECT
+    ANY_VALUE(week_diff) AS week_diff,
+    COUNT(DISTINCT user_pseudo_id) AS first_users
+  FROM week_diff_revisit_users
+  WHERE week_diff = 0
+  AND user_pseudo_id IN (
+    SELECT DISTINCT user_pseudo_id
+    FROM week_diff_revisit_users
+    WHERE first_date NOT IN (SELECT holiday_date FROM holiday)
+  )  -- 재방문 + 연휴 외 유입
+)
+
+
+-- 주차별 리텐션 계산
+SELECT
+    'holiday' AS user_segment,
+    w.week_diff,
+    w.retain_users,
+    f.first_users AS first_users,
+    ROUND(SAFE_DIVIDE(w.retain_users, f.first_users)*100,3) AS retention_rate
+FROM week_retain_holiday_users w
+CROSS JOIN week0_retain_holiday_users f
+
+UNION ALL
+
+-- 주차별 리텐션 계산
+SELECT
+  'normal_day' AS user_segment,
+  w.week_diff,
+  w.retain_users,
+  f.first_users AS first_users,
+  ROUND(SAFE_DIVIDE(w.retain_users, f.first_users)*100,3) AS retention_rate
+FROM week_retain_normal_day_users w
+CROSS JOIN week0_retain_normal_day_users f
+
+ORDER BY user_segment, week_diff
+
